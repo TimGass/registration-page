5cf2e3711fd114cb65728cf15f0360379a8beda4
timgass <timothyagass@gmail.com>
2020-03-10 12:53:25 -0500

    cleaned up user model tests a bit.

--- src/app/models/user.model.spec.ts
+++ src/app/models/user.model.spec.ts
@@ -2,13 +2,16 @@ import { User } from './user.model';
 
 describe('User', () => {
   it('should create an instance.', () => {
-    expect(
-      new User({
-        id: "dj19saqweqweqdasdasdas1-019iwjdqweq10ijd2",
-        username: "yoloman2",
-        email: "yolo2@swag.com",
-        password: "something that would be enhashed"
-      })
-    ).toBeTruthy();
+    const user = new User({
+      id: "dj19saqweqweqdasdasdas1-019iwjdqweq10ijd2",
+      username: "yoloman2",
+      email: "yolo2@swag.com",
+      password: "something that would be enhashed"
+    });
+    expect(user).toBeTruthy();
+    expect(user.id).toBe("dj19saqweqweqdasdasdas1-019iwjdqweq10ijd2");
+    expect(user.username).toBe("yoloman2");
+    expect(user.email).toBe("yolo2@swag.com");
+    expect(user.password).toBe("something that would be enhashed");
   });
 });
